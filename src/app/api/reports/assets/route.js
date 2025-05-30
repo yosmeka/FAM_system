de806dd721e4f09e1a04b3ba3e9bfabb8d7104fb
yosiefdagnachew <2122232425dd@gmail.com>
2025-05-30 11:52:51 -0700

    report-remove-admin

--- src/app/api/reports/assets/route.js
+++ src/app/api/reports/assets/route.js
@@ -4,7 +4,7 @@ import { prisma } from '@/lib/prisma';
 // GET /api/reports/assets
 import { withRole } from '@/middleware/rbac';
 
-export const GET = withRole(['ADMIN', 'MANAGER', 'USER'], async function GET(req) {
+export const GET = withRole([ 'MANAGER', 'USER'], async function GET(req) {
   try {
     const now = new Date();
     const twelveMonthsAgo = new Date();
