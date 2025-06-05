5c68ae7f9805895ed76ffed6e11f432393be21ce
yosiefdagnachew <2122232425dd@gmail.com>
2025-06-05 05:41:38 -0700

    transfer-report-edit

--- src/app/(dashboard)/reports/transfers/page.tsx
+++ src/app/(dashboard)/reports/transfers/page.tsx
@@ -636,25 +636,25 @@ export default function TransferReportsPage() {
                     </div>
                   ),
                 },
-                {
-                  key: 'actions',
-                  header: 'Actions',
-                  render: () => (
-                    <div className="flex justify-center space-x-2">
-                      <button className="p-1 rounded-md hover:bg-gray-100 transition-colors">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-500" viewBox="0 0 20 20" fill="currentColor">
-                          <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-                          <path fillRule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clipRule="evenodd" />
-                        </svg>
-                      </button>
-                      <button className="p-1 rounded-md hover:bg-blue-100 transition-colors">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
-                          <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
-                        </svg>
-                      </button>
-                    </div>
-                  ),
-                },
+                // {
+                //   key: 'actions',
+                //   header: 'Actions',
+                //   render: () => (
+                //     <div className="flex justify-center space-x-2">
+                //       <button className="p-1 rounded-md hover:bg-gray-100 transition-colors">
+                //         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-500" viewBox="0 0 20 20" fill="currentColor">
+                //           <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
+                //           <path fillRule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clipRule="evenodd" />
+                //         </svg>
+                //       </button>
+                //       <button className="p-1 rounded-md hover:bg-blue-100 transition-colors">
+                //         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
+                //           <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                //         </svg>
+                //       </button>
+                //     </div>
+                //   ),
+                // },
               ]}
             />
           </div>
